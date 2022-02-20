e2d2041e3fcd6b0342520c26b7cfce1fb750a6e9
rejwan_ahamed <deadpoolzrx@gmail.com>
2022-02-20 20:00:31 +0600

    first commit

---
+++ php loops/while-loop.php
@@ -0,0 +1,23 @@
+<?php
+
+// while loop increment case
+
+$n = 1;
+
+while ($n <= 10) {
+    echo $n . ". rejwan <br>";
+    $n++;
+}
+
+
+
+// while loop dicrement  case
+
+$n = 10;
+
+while ($n >= 1) {
+    echo $n . ". rejwan2 <br>";
+    $n--;
+}
+
+?>
